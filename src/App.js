3e10a6d7989b9853220e29115e04a5a2235e8920
KarthiV <karthiv@bwefinserv.com>
2026-05-27 17:22:24 +0530

    fix unused ipConfig import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Login from "./components/Login";
 import Register from "./components/Register";
-import ipConfig from "./ipConfig.json";
 
 export const config = {
   endpoint: `https://karthi45675-me-qkart-frontend-v2-master.onrender.com/api/v1`,
