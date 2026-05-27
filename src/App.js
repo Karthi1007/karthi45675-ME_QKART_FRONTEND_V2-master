1de6bc6023af20086f01cddc3e03f4276e7c9ffd
KarthiV <karthiv@bwefinserv.com>
2026-05-27 17:02:39 +0530

    update endpoint  url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Register from "./components/Register";
 import ipConfig from "./ipConfig.json";
 
 export const config = {
-  endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
+  endpoint: `https://karthi45675-me-qkart-frontend-v2-master.onrender.com/api/v1`,
 };
 
 function App() {
